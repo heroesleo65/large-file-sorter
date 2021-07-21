3097d365c5ebd579678e245d83f97dd88c9b0e33
ilya (leo) <heroesleo65@gmail.com>
2021-07-21 12:47:48 +0300

    Rename builder to buffer

--- src/main/java/org/example/utils/StringHelper.java
+++ src/main/java/org/example/utils/StringHelper.java
@@ -78,13 +78,13 @@ public final class StringHelper {
     }
   }
 
-  public static String newString(byte[] values, int count, StringBuilder builder) {
-    builder.ensureCapacity(count);
-    builder.setLength(0);
+  public static String newString(byte[] values, int count, StringBuilder buffer) {
+    buffer.ensureCapacity(count);
+    buffer.setLength(0);
 
     for (int i = 0; i < count; i += 2) {
-      builder.append((char) ((values[i] << 8) + (values[i + 1])));
+      buffer.append((char) ((values[i] << 8) + (values[i + 1])));
     }
-    return builder.toString();
+    return buffer.toString();
   }
 }
