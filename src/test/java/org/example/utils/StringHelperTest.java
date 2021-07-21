2258602e1ab512ad56fda43cf8143a0c9abca6ac
ilya (leo) <heroesleo65@gmail.com>
2021-07-21 12:48:17 +0300

    Add new tests for radix sort

--- src/test/java/org/example/utils/StringHelperTest.java
+++ src/test/java/org/example/utils/StringHelperTest.java
@@ -1,16 +1,22 @@
 package org.example.utils;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class StringHelperTest {
+
+  private static final ClassLoader loader = StringHelperTest.class.getClassLoader();
+
   @SuppressWarnings("unused")
   static Stream<Arguments> getRadixSortParametersProvider() {
     return Stream.of(
@@ -29,6 +35,28 @@ class StringHelperTest {
         Arguments.of(
             new String[] { "aslk", "ablk", "", "v", "tr", "", "variant", "foo", "bar", "foobar" },
             0, 10
+        ),
+        Arguments.of(
+            new String[] {
+                "cama",
+                "casa",
+                "baka",
+                "bala",
+                "bama",
+                "cala",
+                "caea",
+                "aslk",
+                "ablk",
+                "abtk",
+                "ablm",
+                "raea",
+                "raew",
+                "oaew",
+                "oagw",
+                "oagr",
+                "oagt"
+            },
+            0, 17
         )
     );
   }
@@ -41,6 +69,15 @@ class StringHelperTest {
 
     StringHelper.radixSort(values, fromIndex, toIndex);
     assertThat(values).usingElementComparator(String::compareTo).isEqualTo(expected);
+  }
+
+  @ParameterizedTest
+  @CsvSource({"small-input.txt", "middle-input.txt"})
+  void radixSortDataFromFile(String resourceName) throws Exception {
+    String[] text = loadResource(resourceName).lines().toArray(String[]::new);
+
+    StringHelper.radixSort(text, 0, text.length);
+    assertThat(text).isSorted();
   }
 
   @Test
@@ -56,5 +93,12 @@ class StringHelperTest {
 
     assertThatThrownBy(() -> StringHelper.radixSort(new String[] { "", "" }, 3, 2))
         .isInstanceOf(IllegalArgumentException.class);
+  }
+
+  private String loadResource(String name) throws IOException {
+    try (var inputStream = loader.getResourceAsStream(name)) {
+      assert inputStream != null;
+      return new String(inputStream.readAllBytes(), UTF_8);
+    }
   }
 }
