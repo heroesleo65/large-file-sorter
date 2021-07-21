dd607e6acf690aeb22c2c55dcf4e2ecfd2cd1738
ilya (leo) <heroesleo65@gmail.com>
2021-07-21 12:50:07 +0300

    add const INT_IN_LONG_MASK for NumberHelper

--- src/main/java/org/example/utils/NumberHelper.java
+++ src/main/java/org/example/utils/NumberHelper.java
@@ -2,19 +2,21 @@ package org.example.utils;
 
 public final class NumberHelper {
 
+  private static final long INT_IN_LONG_MASK = 0xFFFFFFFFL;
+
   private NumberHelper() {
     throw new UnsupportedOperationException("NumberHelper is utility");
   }
 
   public static long getLong(int a, int b) {
-    return ((long) a << 32L) + ((long) b & 0xFFFFFFFFL);
+    return ((long) a << 32L) + ((long) b & INT_IN_LONG_MASK);
   }
 
   public static int getHiInt(long a) {
-    return (int) ((a >>> 32L) & 0xFFFFFFFFL);
+    return (int) ((a >>> 32L) & INT_IN_LONG_MASK);
   }
 
   public static int getLowInt(long a) {
-    return (int) (a & 0xFFFFFFFFL);
+    return (int) (a & INT_IN_LONG_MASK);
   }
 }
